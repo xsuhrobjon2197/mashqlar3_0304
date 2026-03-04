527c2f1cbd4a5bc401eba04ad9cea9225cc10202
xsuxrobjon2197 <xsuhrobjon2197@gmail.com>
2026-03-04 16:09:58 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+# #1-m
+# def r(a):
+#     if a > 0:
+#         return "musbat"
+#     else:
+#         return "manfiy"
+#
+# x = r(-777)
+# print(x)
+#
+# #2-m
+# def r(a):
+#     if not a % 2  == 0:
+#         return  "Toq"
+#     else:
+#         return "juft"
+#
+# x = r(2)
+# print(x)
+#
+# #3-m
+# def r(a):
+#     if a > 100:
+#         return "katta"
+#     else:
+#         return "kichik"
+#
+# x = r(99999999999999)
+# print(x)
+#
+# #4-misol
+# def r(a,b):
+#     if a == b:
+#         return "teng"
+#     else:
+#         return "teng emas"
+#
+# x = r(2,2)
+# print(x)
+#
+# #5-m
+# def r(a):
+#     if a >= 18:
+#         return "Ruxsat beriladi"
+#     else:
+#         return "Ruxsat berilmadi"
+#
+# x = r(65)
+# print(x)
+#
